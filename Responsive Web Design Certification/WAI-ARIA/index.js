d6b0d4ad30da1d6f93b0770635c4748a017ebece
Kanishk Kumar <96828026+kkr202mlds@users.noreply.github.com>
2026-07-08 15:25:02 +0530

    Create index.js

---
+++ Responsive Web Design Certification/WAI-ARIA/index.js
@@ -0,0 +1,12 @@
+const button = document.getElementById("custom-btn");
+
+button.addEventListener("click", () => {
+  alert("Button clicked!");
+});
+
+button.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" || e.key === " ") {
+    e.preventDefault(); 
+    button.click();
+  }
+});
